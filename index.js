900d6c6cdeeac70496db997249ed6d73b2a9c83c
Victor Karangwa <victorkarangwa4@gmail.com>
2020-09-30 14:44:08 +0200

    add tweets schedulers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Twit = require("twit");
 const dotenv = require("dotenv");
+var schedule = require("node-schedule");
 
 dotenv.config();
 const {
@@ -28,14 +29,38 @@ function retweet() {
         let retweetId = dat.id_str;
         T.post("statuses/retweet/:id", { id: retweetId }, (err, response) => {
           if (response) console.log("Retweeted! " + retweetId);
-          if (err)
-            console.log(
-              'Something went wrong! Maybe a duplicate.'
-            );
+          if (err) console.log("Something went wrong! Maybe a duplicate.");
         });
       }
     }
-  }); 
+  });
 }
 setInterval(retweet, 10000);
 
+schedule.scheduleJob("0 0 0 13 8 *", function () {
+  T.post(
+    "statuses/update",
+    {
+      status:
+        "Today is the birthday of my creator @victorkarangwa4 \n Help me to wish him HAPPY BIRTHDAY  🥳🙌🎂🍰",
+    },
+    function (err, data, response) {
+      if (response) console.log("Tweeted! " + data);
+      if (err) console.log(err);
+    }
+  );
+});
+
+schedule.scheduleJob("0 0 0 1 10 *", function () {
+  T.post(
+    "statuses/update",
+    {
+      status:
+        "May all that you do in this new month be productive and successful. Happy New Month to you all!",
+    },
+    function (err, data, response) {
+      if (response) console.log("Tweeted! " + data);
+      if (err) console.log(err);
+    }
+  );
+});
